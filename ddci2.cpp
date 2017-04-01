29b6f41315007736c226ecb4ccb751841f1dc535
Jasmin Jessich <jasmin@anw.at>
2017-04-01 22:29:10 +0200

    Bump version to 1.0.2

--- ddci2.cpp
+++ ddci2.cpp
@@ -35,7 +35,7 @@
 #include <getopt.h>
 #include <string.h>
 
-static const char *VERSION = "1.0.1";
+static const char *VERSION = "1.0.2";
 static const char *DESCRIPTION = "External Digital Devices CI-Adapter";
 
 static const char *DEV_DVB_CI = "ci";
